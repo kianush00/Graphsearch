17615d3eb5c4fd7729778ce949a6ed7f199141ee
kianush00 <k.atighimoghadda01@ufromail.cl>
2024-05-30 04:41:54 -0400

    Added BinaryExpressionTree and BinaryTreeNode classes in srex_new_classes.py

--- jupyter/srex_new_classes.py
+++ jupyter/srex_new_classes.py
@@ -16,6 +16,7 @@ from numpy import dot
 from enum import Enum
 import copy
 from collections import deque 
+import re
 
 
 class TextUtils:
@@ -104,6 +105,338 @@ class TextUtils:
         transformed_text = ' '.join(filtered_sentence)
         
         return transformed_text
+
+
+
+class BinaryTreeNode:
+    def __init__(self, value: str):
+        self.value = value
+        self.graph = None
+        self.left: BinaryTreeNode | None = None
+        self.right: BinaryTreeNode | None = None
+    
+
+    def is_leaf(self) -> bool:
+        is_leaf = not self.left and not self.right
+        return is_leaf
+    
+
+    def get_values_from_leaves(self) -> list[str]:
+        if self.is_leaf():
+            return [self.value]
+        return self.left.get_values_from_leaves() + self.right.get_values_from_leaves()
+    
+
+    def __str__(self) -> str:
+        return ' '.join(self.inorder_traversal())
+    
+
+    def tree_str(self) -> str:
+        return self.generate_tree_str()
+    
+
+    def inorder_traversal(self) -> list[str]:
+        result = []
+        if self.left:
+            result.extend(self.left.inorder_traversal())
+        result.append(self.value)
+        if self.right:
+            result.extend(self.right.inorder_traversal())
+        return result
+    
+
+    def generate_tree_str(self, level: int = 0) -> str:
+        output = "\t" * level + self.value + "\n"
+        if self.left:
+            output += self.left.generate_tree_str(level + 1)
+        if self.right:
+            output += self.right.generate_tree_str(level + 1)
+        return output
+
+
+
+class BinaryExpressionTree:
+    def __init__(self, raw_query: str):
+        self.__raw_query = raw_query
+        self.root: BinaryTreeNode | None = None
+        
+        try:
+            tokens = self.__separate_boolean_query(self.__raw_query)
+            infix_tokens = self.__process_infix_tokens(tokens)
+            postfix_tokens = self.__infix_to_postfix(infix_tokens)
+            self.__construct_tree_from_postfix(postfix_tokens)
+        except Exception as e:
+            print('Error: ' + repr(e))
+    
+
+    def get_raw_query(self):
+        return self.__raw_query
+    
+
+    def get_query_terms(self) -> list[str]:
+        if self.root:
+            return self.root.get_values_from_leaves()
+    
+
+    def do_text_transformations_to_query_terms(self, 
+            stop_words_list: list[str] = [], 
+            lema: bool = True, 
+            stem: bool = True
+            ) -> None:
+        """
+        Apply text transformations to the terms in the leaf nodes.
+
+        Parameters
+        ----------
+        stop_words_list : list[str]
+            List of stop words to be removed from the terms
+        lema : bool
+            If True, lemmatization is applied
+        stem : bool
+            If True, stemming is applied
+        """
+        def transform_node_if_leaf(node: BinaryTreeNode) -> None:
+            if node.is_leaf():
+                node.value = TextUtils.get_transformed_text_if_it_has_underscores(node.value, stop_words_list, lema, stem)
+            else:
+                transform_node_if_leaf(node.left)
+                transform_node_if_leaf(node.right)
+
+        if self.root:
+            transform_node_if_leaf(self.root)
+    
+
+    def initialize_graph_for_all_nodes(self, 
+            nr_of_graph_terms: int = 5, 
+            limit_distance: int = 4, 
+            include_refterms: bool = True
+            ) -> None:
+        """
+        Initialize graphs associated to each node in the tree.
+
+        Parameters
+        ----------
+        nr_of_graph_terms : int
+            Configured number of terms in the graph
+        limit_distance : int
+            Maximal distance of terms used to calculate the vicinity
+        include_reference_term : bool
+            If True, the reference term is included in the vicinity
+        """
+        def initialize_graph(node: BinaryTreeNode):
+            #node.graph = VicinityGraph(nr_of_graph_terms, limit_distance, include_refterms)
+            node.graph = (nr_of_graph_terms, limit_distance, include_refterms)
+            print(node.graph)
+            if not node.is_leaf():
+                initialize_graph(node.left)
+                initialize_graph(node.right)
+        
+        if self.root:
+            initialize_graph(self.root)
+
+
+    def __str__(self) -> str:
+        if not self.root:
+            return ''
+        return ' '.join(self.root.inorder_traversal())
+    
+
+    def tree_str(self) -> str:
+        if not self.root:
+            return ''
+        return self.root.generate_tree_str()
+    
+
+    def __separate_boolean_query(self, query: str) -> list[str]:
+        # Define a regex pattern to match boolean operators, parentheses, colons and terms
+        pattern = r'(\bAND\b|\bOR\b|\bNOT\b|\(|\)|\w+|:)'
+        
+        # Find all matches using the pattern
+        tokens = re.findall(pattern, query)
+        
+        return tokens
+    
+
+    def __process_infix_tokens(self, tokens: list[str]) -> list[str]:
+        validation_tuple = self.__validate_boolean_expression(tokens)
+        if validation_tuple[0]:
+            tokens_deleted_not_expressions = self.__process_boolean_tokens_delete_not_expressions(tokens)
+            tokens_deleted_colon_and_keys = self.__process_boolean_tokens_delete_colon_and_keys(tokens_deleted_not_expressions)
+            infix_tokens = self.__join_term_tokens_by_underscores(tokens_deleted_colon_and_keys)
+            return infix_tokens
+        else:
+            raise Exception(validation_tuple[1])
+    
+
+    def __infix_to_postfix(self, infix_tokens: list[str]) -> list[str]:
+        # Operator precedence
+        precedence = {'OR': 1, 'AND': 2}
+        # Stack for operators
+        operator_stack = deque()
+        # List for expression in postfix
+        postfix_list = []
+
+        for token in infix_tokens:
+            if token in precedence.keys():
+                # If the token is an operator, handle precedence (operator_stack[0] is the top element of stack)
+                while (operator_stack and operator_stack[0] != '(' and
+                    precedence[operator_stack[0]] >= precedence[token]):
+                    postfix_list.append(operator_stack.popleft())
+                operator_stack.appendleft(token)
+            elif token == '(':
+                # If the token is '(', add it to the operator stack
+                operator_stack.appendleft(token)
+            elif token == ')':
+                # If the token is ')', pop until find '(' (operator_stack[0] is the top element of stack)
+                while operator_stack and operator_stack[0] != '(':
+                    postfix_list.append(operator_stack.popleft())
+                operator_stack.popleft()  # Pop '(' from stack
+            else:
+                # If the token is an operand (alphanumeric word or containing '_'), add it to postfix
+                postfix_list.append(token)
+
+        # Add any remaining operators on the stack to the postfix list
+        while operator_stack:
+            postfix_list.append(operator_stack.popleft())
+
+        return postfix_list
+    
+    
+    def __construct_tree_from_postfix(self, postfix_tokens: list[str]) -> None:
+        stack = []
+        for token in postfix_tokens:
+            if token in ['AND', 'OR']:
+                # If the token is an operator, create a node and link the subtrees
+                node = BinaryTreeNode(token)
+                node.right = stack.pop()
+                node.left = stack.pop()
+                stack.append(node)
+            else:
+                # If the token is an operand, create a node and add it to the stack
+                node = BinaryTreeNode(token)
+                stack.append(node)
+        
+        # The last element on the stack is the root of the tree
+        self.root = stack.pop()
+
+
+    def __validate_boolean_expression(self, tokens: list[str]) -> tuple[bool, str]:
+        # Check parenthesis syntax
+        stack = []
+        for token in tokens:
+            if token == '(':
+                stack.append(token)
+            elif token == ')':
+                if not stack:
+                    return False, "Unmatched closing parenthesis"
+                stack.pop()
+        if stack:
+            return False, "Unmatched opening parenthesis"
+
+        # Check AND OR operators
+        operators = {'AND', 'OR'}
+        length = len(tokens)
+        
+        for i, token in enumerate(tokens):
+            if token in operators:
+                if i == 0 or i == length - 1:
+                    return False, f"Operator {token} cannot be at the start or end of the expression"
+                if tokens[i-1] in operators or tokens[i+1] in operators:
+                    return False, f"Operator {token} must be surrounded by valid terms"
+
+        return True, "The expression is valid"
+
+
+    def __process_boolean_tokens_delete_not_expressions(self, tokens: list[str]) -> list[str]:
+        indices_to_exclude = []
+        index = 0
+
+        while index < len(tokens):
+            next_index = index + 1
+            if tokens[index] == 'NOT':
+                stack = 0
+                while next_index < len(tokens):
+                    if tokens[next_index] == '(':
+                        stack += 1
+                    elif tokens[next_index] == ')':
+                        if stack < 2:   # if it is the last closing parenthesis, or there is no opening parenthesis between the two indices
+                            if stack == 1:
+                                next_index += 1     # removes the closing parenthesis if there was an opening parenthesis before
+                            break
+                        else:
+                            stack -= 1
+                    elif tokens[next_index] in ['AND', 'OR', 'NOT']:
+                        if stack < 1:   # if there was no opening parenthesis between the two indices
+                            break
+                    next_index += 1
+                indices_to_exclude.extend(range(index, next_index))  # add the indexes to delete their values
+            index = next_index
+        
+        processed_tokens = [token for index, token in enumerate(tokens) if index not in indices_to_exclude]
+        return processed_tokens
+
+
+    def __process_boolean_tokens_delete_colon_and_keys(self, tokens: list[str]) -> list[str]:
+        prev_index = 0
+        indices_to_remove = []
+        for index, token in enumerate(tokens):
+            if token == ':':
+                prev_index = index
+                while prev_index > 0:
+                    if tokens[prev_index - 1] in ['AND', 'OR', '(']:
+                        break
+                    else:
+                        prev_index -= 1
+                indices_to_remove.extend(range(prev_index, index+1))  # add the indexes to delete their values
+        processed_tokens = [token for index, token in enumerate(tokens) if index not in indices_to_remove]
+        return processed_tokens
+
+
+    def __join_term_tokens_by_underscores(self, tokens: list[str]) -> list[str]:
+        if len(tokens) <= 1:
+            return tokens
+        else:
+            _2d_matrix_to_join_by_underscore = []
+            for index, token in enumerate(tokens):
+                if (token in ['AND', 'OR', ')']) or (index == (len(tokens)-1)):  #if token is operator or its the last element
+                    prev_index = index - 1
+                    while prev_index >= 0:
+                        if tokens[prev_index] in ['AND', 'OR', '(', ')']:
+                            prev_index += 1
+                            break
+                        if prev_index == 0:
+                            break
+                        prev_index -= 1
+                    last_index = index - 1 if token in ['AND', 'OR', ')'] else index
+                    tuple_to_join_by_underscore = (prev_index, last_index)
+                    if (tuple_to_join_by_underscore[1] - tuple_to_join_by_underscore[0]) > 0:
+                        _2d_matrix_to_join_by_underscore.append(tuple_to_join_by_underscore)
+            
+            current_index = 0
+            processed_tokens = []
+            for start, end in _2d_matrix_to_join_by_underscore:
+                # Make sure indexes are valid
+                if start < 0 or end >= len(tokens) or start > end:
+                    raise ValueError(f"Índices inválidos para unir elementos: {start}, {end}")
+
+                # Add the token elements before the current subset
+                while current_index < start:
+                    processed_tokens.append(tokens[current_index])
+                    current_index += 1
+
+                # Join subset elements with underscores
+                joined_element = '_'.join(tokens[start:end+1])
+                processed_tokens.append(joined_element)
+
+                # Move the current index to the end of the subset
+                current_index = end + 1
+            
+            # Agregar los elementos restantes de tokens después del último subconjunto
+            while current_index < len(tokens):
+                processed_tokens.append(tokens[current_index])
+                current_index += 1       
+
+        return processed_tokens
 
 
 
