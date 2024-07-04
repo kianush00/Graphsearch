2b20f977e161507769ddc5ab8bc29aa24d849714
kianush00 <k.atighimoghadda01@ufromail.cl>
2024-07-04 02:28:39 -0400

    Configured frontend for npm run build and npm start, now the source code is in src/ directory and its compile result through webpack remains in the directory dist/, for javascript and css output files

--- frontend/README.md
+++ frontend/README.md
@@ -3,12 +3,31 @@
 This repository contains the frontend of the SREX project.
 
 
-# Run HTTP Server
+# Install dependencies
 
-For running the HTTP Server, execute the following command:
+To install frontend dependencies, use the following command:
 
 ```bash
-    python -m http.server 3000
+    npm install
 ```
 
+
+# Build the project
+
+To use TypeScript, the files located in the ./src directory must be compiled with output in ./dist. To do this, just run the following command:
+
+```bash
+    npm run build
+```
+
+
+# Run HTTP Server
+
+1. For running the HTTP Server, execute the following command:
+
+```bash
+    npm start
+```
+
+2. Then go to [http://localhost:3000](http://localhost:3000)
 
---
+++ frontend/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
+};
